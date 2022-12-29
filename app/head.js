82d9732667b1838f138afa6180598e34c2fc94bf
Radomir Mijovic <radomir.mijovic@productdna.com>
2022-12-29 18:59:47 +0100

    feat: added logo

--- app/head.js
+++ app/head.js
@@ -4,8 +4,8 @@ export default function Head() {
             <title>Hatha Yoga Budva</title>
             <meta content="width=device-width, initial-scale=1" name="viewport"/>
             <link rel="canonical" href="https://www.hathayogabudva.me/"/>
-            <link rel="icon" href="/favicon.ico"/>
-            <meta name="keywords" content="yoga, joga, hatha joga, hatha yoga, Хатха-йога"/>
+            <link rel="icon" href="/images/icons/lotus.svg"/>
+            <meta name="keywords" content="yoga, joga budva, yoga budva, joga crna gora, joga, hatha joga, hatha yoga, Хатха-йога"/>
             <meta name='description'
                   content='Hatha yoga can be considered an umbrella term to describe many of the most common forms of yoga taught in the West today. With this type of yoga, you move your body slowly and deliberately into different poses that challenge your strength and flexibility, while at the same time focusing on relaxation and mindfulness.'/>
         </>
